29cab94b9d449cc6af85e291dfd1c5fb00b5acf5
Anurag Dutta <imanurag18@gmail.com>
2023-07-29 15:22:21 +0530

    changed

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
-import services from '../../public/services.js';
-
 const Services = () => {
+    const services = [
+    
+        {id:1, name:'WEB DEVELOPMENT', description:'I am a MERN stack Web developer with an expertise in ReactJS and NodeJS.', image:'../src/assets/code.png'},
+        {id:2, name:'COMPETITIVE CODING', description:'I am a 5* coder at Hackerrank and have solved over 100 questions at gfg and currently solving leetcode. The Languages I can use are Java, C and Python.', image:'../src/assets/android.png'},
+        {id:3, name:'OPEN-SOURCE CONTRIBUTOR', description:'I have over 300+ contributions in Github and have participated in open source events like Hacktoberfest, GSSOC and SWOC.', image:'../src/assets/git-repo.png'},
+    ];
     return (
         <div>
               <h1 className="text-primary font-semibold text-3xl mt-14">My Area of expertise:-</h1>
