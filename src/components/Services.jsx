be5f18866a14750ab6fdef5c2a074e4af109ae0b
Anurag Dutta <imanurag18@gmail.com>
2023-07-30 00:47:47 +0530

    changed

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Services = () => {
     const services = [
     
-        {id:1, name:'WEB DEVELOPMENT', description:'I am a MERN stack Web developer with an expertise in ReactJS and NodeJS.', image:'./src/assets/code.png'},
-        {id:2, name:'COMPETITIVE CODING', description:'I am a 5* coder at Hackerrank and have solved over 100 questions at gfg and currently solving leetcode. The Languages I can use are Java, C and Python.', image:'./src/assets/android.png'},
-        {id:3, name:'OPEN-SOURCE CONTRIBUTOR', description:'I have over 300+ contributions in Github and have participated in open source events like Hacktoberfest, GSSOC and SWOC.', image:'./src/assets/git-repo.png'},
+        {id:1, name:'WEB DEVELOPMENT', description:'I am a MERN stack Web developer with an expertise in ReactJS and NodeJS.', image:'images/code.png'},
+        {id:2, name:'COMPETITIVE CODING', description:'I am a 5* coder at Hackerrank and have solved over 100 questions at gfg and currently solving leetcode. The Languages I can use are Java, C and Python.', image:'images/android.png'},
+        {id:3, name:'OPEN-SOURCE CONTRIBUTOR', description:'I have over 300+ contributions in Github and have participated in open source events like Hacktoberfest, GSSOC and SWOC.', image:'images/git-repo.png'},
     ];
     return (
         <div>
